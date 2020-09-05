343497065b7e048a06a7d56a4836f8a7899e2f3a
hypercc <charliecondorcet@gmail.com>
2020-09-05 18:57:49 -0400

    docs(SqlUniversalisRepo): update all documentation

--- Data/SqlUniversalisRepo.cs
+++ Data/SqlUniversalisRepo.cs
@@ -4,18 +4,40 @@ using System.Linq;
 
 namespace Universalis.Data
 {
+    /// <summary>
+    /// Sql Server database connection
+    /// </summary>
     public class SqlUniversalisRepo : IUniversalisRepo
     {
+
+        /// <summary>
+        /// The context
+        /// </summary>
         private readonly UniversalisContext _context;
+
+        /// <summary>
+        /// The constructor with dependency injection
+        /// </summary>
+        /// <param name="context">the context</param>
         public SqlUniversalisRepo(UniversalisContext context)
         {
             this._context = context;
         }
+
+        /// <summary>
+        /// Find the first Academic by id
+        /// </summary>
+        /// <param name="id">id to search</param>
+        /// <returns>Academic by id</returns>
         public Academic GetAcademicById(int id)
         {
             return this._context.Academics.FirstOrDefault(a => a.Id == id);
         }
 
+        /// <summary>
+        /// All Academics from DB
+        /// </summary>
+        /// <returns></returns>
         public IEnumerable<Academic> GetAllAcademics()
         {
             return this._context.Academics.ToList();
--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,8 @@ namespace Universalis
 
             services.AddControllers();
 
-            services.AddScoped<IUniversalisRepo, MockAcademicRepo>();
+            // add implementation to sql server db
+            services.AddScoped<IUniversalisRepo, SqlUniversalisRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
