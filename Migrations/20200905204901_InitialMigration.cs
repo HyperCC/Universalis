8e9133f19a4a7e1dc5afab159f6eb251fe8156fd
hypercc <charliecondorcet@gmail.com>
2020-09-05 17:12:57 -0400

    feat(Migrations/): add initial migration

---
+++ Migrations/20200905204901_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Universalis.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Academics",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(nullable: false),
+                    Cargo = table.Column<string>(nullable: false),
+                    Unidad = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    Telefono = table.Column<string>(nullable: true),
+                    Oficina = table.Column<string>(nullable: true),
+                    DireccionTrabajo = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Academics", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Academics");
+        }
+    }
+}
